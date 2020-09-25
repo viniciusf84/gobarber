4cb0d74a23a2d0386d61f06b2396b0885eb33a8b
inicius <viniciusf84@hotmail.com>
2020-09-24 23:48:13 -0300

    Add web folder

---
+++ web/src/components/Tooltip/styles.ts
@@ -0,0 +1,42 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  position: relative;
+
+  span {
+    width: 160px;
+    background: #ff9000;
+    padding: 8px;
+    border-radius: 4px;
+    font-size: 14px;
+    font-weight: 500;
+    opacity: 0;
+    transition: opacity 0.4s ease-in-out;
+    visibility: hidden;
+
+    position: absolute;
+    bottom: calc(100% + 12px);
+    left: 50%;
+    transform: translateX(-50%);
+
+    color: #312e38;
+
+    &::before {
+      content: '';
+      width: 0;
+      height: 0;
+      position: absolute;
+      border-style: solid;
+      border-color: #ff9000 transparent;
+      border-width: 6px 6px 0 6px;
+      bottom: 20px;
+      top: 100%;
+      left: 50%;
+    }
+  }
+
+  &:hover span {
+    opacity: 1;
+    visibility: visible;
+  }
+`;
